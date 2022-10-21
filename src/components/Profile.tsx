051a6204930a881c3834ab9bf2a638598c95254a
IvanKuper <ivankuper7@gmail.com>
2022-10-21 17:13:53 +0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,66 @@
+import { FunctionComponent, useEffect, useState } from "react";
+import { errorMsg } from "../services/feedbackService";
+import { getUser } from "../services/userService";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+interface ProfileProps {}
+
+const Profile: FunctionComponent<ProfileProps> = () => {
+  const [userProfile, setUserProfile] = useState<any>({
+    id: "",
+    name: "",
+    email: "",
+    biz: false,
+  });
+  useEffect(() => {
+    getUser()
+      .then((result) => {
+        setUserProfile(result.data);
+      })
+      .catch((err) => {
+        errorMsg("Something went Wrong, Try Agian");
+      });
+  }, []);
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-center mt-5 mb-3">
+        <i className="fa-solid fa-user mb-4"></i> Profile
+      </h1>
+      <div className=" container-fluid content">
+        <div className="containerDiv card">
+          <img
+            src="banner.jpg"
+            alt="cookies"
+            className="hero-image img-fluid"
+          />
+
+          <div className="information">
+            <img src="avatar.png" alt="avatar" className="avatar" />
+            <span className="mb-4">Currently Signed In As:</span>
+            <div className="name mb-3 ">{userProfile.name}</div>
+            <hr />
+            <div className="mb-2">
+              <strong>Email: </strong>
+              {userProfile.email}
+            </div>
+            <div className="mb-2">
+              <strong>Account Type: </strong>
+              {userProfile.biz ? (
+                <span style={{ color: "green" }}>
+                  Business Account <i className="fa-solid fa-square-check"></i>{" "}
+                </span>
+              ) : (
+                <span>Free Account </span>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Profile;
